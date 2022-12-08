696f8d2008522a05d45e43eba31a50d9744ff474
Jon Finerty <jonathanfinerty@gmail.com>
2022-12-08 11:03:19 +0000

    Day 1, second star

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,22 +1,20 @@
 ﻿var lines = File.ReadLinesAsync("input.txt");
 
-var max = 0;
-var maxElfIndex = 0;
-var currentElfIndex = 0;
+
+var topThreeTotals = new List<int>(new int[]{0,0,0});
 var currentElfTotal = 0;
 
 await foreach (var line in lines) {
     if (string.IsNullOrWhiteSpace(line)) {
-        if (currentElfTotal > max) {
-            max = currentElfTotal;
-            maxElfIndex = currentElfIndex;
-        }
-        
+        topThreeTotals.Add(currentElfTotal);
+        topThreeTotals.Sort();
+        topThreeTotals.RemoveAt(0);        
         currentElfTotal = 0;
-        currentElfIndex++;
         continue;
     }
     currentElfTotal += int.Parse(line);
 }
 
-Console.WriteLine($"Max input: {max} at Elf Index: {maxElfIndex}");
+Console.WriteLine($"Max input: {topThreeTotals.Last()}");
+
+Console.WriteLine($"Top three values: {topThreeTotals.Sum()}");
