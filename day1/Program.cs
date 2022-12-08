6bbfba7cc9ad346b9eeba133bda7061fbee7f838
Jon Finerty <jonathanfinerty@gmail.com>
2022-12-08 10:50:06 +0000

    Add day1 script

---
+++ day1/Program.cs
@@ -0,0 +1,22 @@
+﻿var lines = File.ReadLinesAsync("input.txt");
+
+var max = 0;
+var maxElfIndex = 0;
+var currentElfIndex = 0;
+var currentElfTotal = 0;
+
+await foreach (var line in lines) {
+    if (string.IsNullOrWhiteSpace(line)) {
+        if (currentElfTotal > max) {
+            max = currentElfTotal;
+            maxElfIndex = currentElfIndex;
+        }
+        
+        currentElfTotal = 0;
+        currentElfIndex++;
+        continue;
+    }
+    currentElfTotal += int.Parse(line);
+}
+
+Console.WriteLine($"Max input: {max} at Elf Index: {maxElfIndex}");
